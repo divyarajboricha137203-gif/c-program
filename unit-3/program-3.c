5b6c196bc7a3f2451ed34baed3d000c35317dd26
divyarajboricha137203-gif <divyaraj.boricha137203@marwadiuniversity.ac.in>
2026-04-01 10:26:32 +0530

    Add files via upload

---
+++ unit-3/program-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+void main()
+{
+int a =100; int b=200;
+printf("Before swap, value of a:%d\n",a);
+printf("Before swap,value of b : %d\n", b);
+swap
+}
